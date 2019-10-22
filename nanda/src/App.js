11e91d2df24c10822588481fdb18bd27fc102ddd
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2019-10-22 15:21:54 -0700

    routes aren't private but working

--- nanda/src/App.js
+++ nanda/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import PrivateRoute from './utils/PrivateRoute';
 
+import LandingPage from './components/LandingPage';
 import ParentDashboard from './components/ParentDashboard';
 import NannyDashboard from './components/NannyDashboard';
 import ParentLogin from './components/auth/ParentLogin';
@@ -14,43 +15,40 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>NANDA</h1>
-     <Router>
-      
-      <ul>
+      <Router>
+      <Link exact to="/">NANDA</Link>
+        {/* <ul>
         <li>
-          <Link to="/parent">Parent Member Sign In</Link>
-        </li>
-        <li>
-          <Link to="/nanny">Nanny Member Sign In</Link>
-        </li>
-        <li>
-          <Link to="/nannysignup">I want to use Nanda for my Nanny service scheduling!</Link>
-        </li>
-        <li>
-          <Link to="/parentsignup">I am a Parent that needs assistance with my kids!</Link>
-        </li>
-      </ul>
-      <Route path="/parent" component={ParentLogin} />
-      <Route path="/nanny" component={NannyLogin} />
+            <Link exact to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/parent">Parent Member Sign In</Link>
+          </li>
+          <li>
+            <Link to="/nanny">Nanny Member Sign In</Link>
+          </li>
+          <li>
+            <Link to="/nannysignup">I want to use Nanda for my Nanny service scheduling!</Link>
+          </li>
+          <li>
+            <Link to="/parentsignup">I am a Parent that needs assistance with my kids!</Link>
+          </li>
+        </ul> */}
 
-      <Route exact path='/parentsignup' component={ParentForm}/>
-      <Route exact path='/nannysignup' component={NannyForm}/>
-      {/* <Route exact path="/" component={ParentDashboard} /> */}
-      {/* If the user is not logged in, the Private Route will redirect to the Login */}
-      {/* 
-      user.isNanny === true ? render={<PrivateRoute exact path="/" component={NannyDashboard} />} : <PrivateRoute exact path="/" component={ParentDashboard} />
-      */}
-      <PrivateRoute exact path="/p" component={ParentDashboard}  />
-      <PrivateRoute exact path="/n" component={NannyDashboard}  />
-     </Router>
-     
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/parent" component={ParentLogin} />
+          <Route exact path="/nanny" component={NannyLogin} />
+
+          <Route exact path='/parentsignup' render={() => <ParentForm />}/>
+          <Route exact path='/nannysignup' component={NannyForm}/>
+          {/* <PrivateRoute exact path="/p" component={ParentDashboard}  />
+          <PrivateRoute exact path="/n" component={NannyDashboard}  /> */}
+        </Switch>
+      </Router>
     </div>
   );
 };
-
-
-
+        
 
 export default App;
-
