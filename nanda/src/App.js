3b3b50dbd4dd456866f8ae45dddf1c1340f7f322
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2019-10-23 06:25:22 -0700

    wed morning

--- nanda/src/App.js
+++ nanda/src/App.js
@@ -17,24 +17,6 @@ function App() {
     <div className="App">
       <Router>
       <Link exact to="/">NANDA</Link>
-        {/* <ul>
-        <li>
-            <Link exact to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/parent">Parent Member Sign In</Link>
-          </li>
-          <li>
-            <Link to="/nanny">Nanny Member Sign In</Link>
-          </li>
-          <li>
-            <Link to="/nannysignup">I want to use Nanda for my Nanny service scheduling!</Link>
-          </li>
-          <li>
-            <Link to="/parentsignup">I am a Parent that needs assistance with my kids!</Link>
-          </li>
-        </ul> */}
-
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route exact path="/parent" component={ParentLogin} />
@@ -42,13 +24,10 @@ function App() {
 
           <Route exact path='/parentsignup' render={() => <ParentForm />}/>
           <Route exact path='/nannysignup' component={NannyForm}/>
-          {/* <PrivateRoute exact path="/p" component={ParentDashboard}  />
-          <PrivateRoute exact path="/n" component={NannyDashboard}  /> */}
         </Switch>
       </Router>
     </div>
   );
 };
         
-
 export default App;
